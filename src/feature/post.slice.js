28f4c83831c99efea11903fa5e4da93438e861bc
curlyroots <selimcodeur@gmail.com>
2022-06-04 17:33:59 +0200

    method post for table comments ok

--- src/feature/post.slice.js
+++ src/feature/post.slice.js
@@ -17,8 +17,20 @@ export const postSlice = createSlice({
     deletePost: (state, { payload }) => {
       state.posts = state.posts.filter((pic) => pic.id !== payload);
     },
+    addComment: (state, { payload }) => {
+      state.posts = state.posts.map((post) => {
+        if (post.id === payload[0]) {
+          return {
+            ...post,
+            comments: payload[1],
+          };
+        } else {
+          return post;
+        }
+      });
+    },
   },
 });
 
-export const { getPosts, addPost, deletePost } = postSlice.actions;
+export const { getPosts, addPost, deletePost, addComment } = postSlice.actions;
 export default postSlice.reducer;
